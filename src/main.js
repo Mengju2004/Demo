3292816e21bdffb3c61ce51f173ce5bf6538df78
zjf <304579432@qq.com>
2020-08-31 21:28:00 +0800

    zjf-20200831-mine

--- src/main.js
+++ src/main.js
@@ -2,20 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'amfe-flexible'
+import './assets/style/reset.scss'
 
+import 'amfe-flexible'
 
 //引入vant组件
 import {
     Button,
     Tabbar,
-    TabbarItem
+    TabbarItem,
+    Cell,
+    CellGroup
 } from 'vant'
 
 
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
 Vue.use(Button);
+Vue.use(Cell);
+Vue.use(CellGroup);
 
 Vue.config.productionTip = false
 
