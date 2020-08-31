30c54b52f9bc9b5f77f968cfb45c871470cfef24
Mengju2004 <394526056@qq.com>
2020-08-31 21:13:44 +0800

    tabbar

---
+++ public/icons/tabBarVar.js
@@ -0,0 +1,14 @@
+export const icon01active = require('./icon_01@1.png')
+export const icon01inactive = require('./icon_01@0.png')
+
+export const icon02active = require('./icon_02@1.png')
+export const icon02inactive = require('./icon_02@0.png')
+
+export const icon03active = require('./icon_03@1.png')
+export const icon03inactive = require('./icon_03@0.png')
+
+export const icon04active = require('./icon_04@1.png');
+export const icon4inactive = require('./icon_04@0.png')
+
+export const icon05active = require('./icon_05@1.png')
+export const icon05inactive = require('./icon_05@0.png')
