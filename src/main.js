b8c7abe722fa9fa0f35d0df6366a0ea77f37cdaa
jiangxin-max <1750912515@qq.com>
2020-08-31 22:21:24 +0800

    login-20.8.31-zhouqiang

--- src/main.js
+++ src/main.js
@@ -3,19 +3,28 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
-
+//公共重置样式
+import './assets/style/reset.scss'
+//字体图标css引入
+import './assets/iconfont/iconfont.css'
 
 //引入vant组件
 import {
     Button,
     Tabbar,
-    TabbarItem
+    TabbarItem,
+    Field,
+    Checkbox,
+    CheckboxGroup
 } from 'vant'
 
 
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
 Vue.use(Button);
+Vue.use(Field);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,32 +4,40 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path: '/',
-        component: () => import("@/views/Home"),
-        redirect:'/index',
-        children: [
-            {
-                path: '/index',
-                component: () => import("@/views/Index")
-            }, {
-                path: '/sleep',
-                component: () => import("@/views/Sleep")
-            }, {
-                path: '/med',
-                component: () => import("@/views/Meditation")
-            }, {
-                path: '/music',
-                component: () => import("@/views/Music")
-            }, {
-                path: '/mine',
-                component: () => import("@/views/Mine")
-            }
+const routes = [{
+    path: '/',
+    component: () =>
+        import ("@/views/Home"),
+    redirect: '/index',
+    children: [{
+            path: '/index',
+            component: () =>
+                import ("@/views/Index")
+        }, {
+            path: '/sleep',
+            component: () =>
+                import ("@/views/Sleep")
+        }, {
+            path: '/med',
+            component: () =>
+                import ("@/views/Meditation")
+        }, {
+            path: '/music',
+            component: () =>
+                import ("@/views/Music")
+        }, {
+            path: '/mine',
+            component: () =>
+                import ("@/views/Mine")
+        },
+        {
+            path: '/login',
+            component: () =>
+                import ('@/views/Login')
+        }
 
-        ]
-    }
-]
+    ]
+}]
 
 const router = new VueRouter({
     routes
