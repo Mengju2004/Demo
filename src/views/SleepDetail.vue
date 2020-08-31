74af4d2ced568b2cc7b742ea16fb4b320bf518ef
cakeyI <2205402063@qq.com>
2020-08-31 22:20:04 +0800

    睡眠页面及睡眠详情

---
+++ src/views/SleepDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="s-detail">
+      <div class="detail-text">
+          <p></p>
+          <div>
+              <img src="" alt="">
+              <div>
+                  <p><span>限免</span></p>
+                  <p>9<span>课时</span>·<span>3462</span>人练习</p>
+                  <p></p>
+              </div>
+          </div>
+      </div>
+      <nav>
+          <li>
+              <span></span>
+              <div>
+                  <p></p>
+                  <p>时长<span></span></p>
+              </div>
+              <span></span>
+          </li>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="scss">
+.s-detail{
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: magenta;
+    z-index: 10;
+}
+</style>
